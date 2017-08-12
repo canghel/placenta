5ccf30d5ab399a82d414e8dfb431bced7cea0c88
canghel <cvanghel@gmail.com>
2017-08-12 15:49:42 -0700

    updated to make background white and crop

--- scripts/data.setup.initial.py
+++ scripts/data.setup.initial.py
@@ -1,3 +1,13 @@
+# Preprocess files:
+# - create a white mask around the placenta
+# - erase the perimeter from the trace pictures
+# - make traces black and white
+# - create skeleton traces (no thickness for vessels): still under construction
+
+# Reminder to self:
+# 0 = black
+# 255 = white
+
 ### PREAMBLE ##################################################################
 
 import numpy as np
@@ -10,9 +20,12 @@ import scipy.ndimage
 
 ### PATHS #####################################################################
 
-pathPhotos = "./photos"
-pathTraces = "./traces"
-pathSkeletons = "./skeletons"
+pathPhotos = "../Photos/Raw-photos"
+pathTraces = "../Traces/Raw-traces"
+pathSkeletons = "../Skeletons"
+
+pathPhotosProcessed = "../Photos/Pre-processed"
+pathTracesProcessed = "../Traces/Pre-processed"
 
 ### GET NAMES OF FILES ########################################################
 
@@ -22,33 +35,72 @@ skelFiles =  [f for f in listdir(pathSkeletons) if isfile(join(pathSkeletons, f)
 
 numFiles = len(photoFiles);
 
-# load photo and tracing files
-# crop to only have the placenta area
-# make tracing bw 
-### CROP FILES ################################################################
+### OBTAIN A MASK #############################################################
 
-# load one file
-# for jj in range(0, numFiles):
-jj = 0
-print('--- Working on file '+str(jj+1)+' --------------------------------')
-#filenameStem = traceFiles[jj]
-#filenameStem = filenameStem.split('.')[0]
-# print(filenameStem)
+# load each file
+for jj in range(0, numFiles):
+    
+    print('--- Working on file '+str(jj+1)+' --------------------------------')
+    filenameStem = traceFiles[jj]
+    filenameStem = filenameStem.split('.')[0]
+    print(filenameStem)
 
-# load a tracing
-#traceImagePath = os.path.join(pathTraces, traceFiles[jj])
-traceImagePath = './T000-BN0013990_fetalsurface_fixed_ruler_lights_filter_12_0130-dd.png'
-traceImage = cv2.imread(traceImagePath)
+    # load a tracing
+    traceImagePath = os.path.join(pathTraces, traceFiles[jj])
+    traceImage = cv2.imread(traceImagePath)
 
-# the perimeter indices have rgb code (0, 255, 0)
-perimIndices = np.where(np.all(traceImage == [0, 255, 0], axis=-1))
-coords = np.column_stack((perimIndices[1], perimIndices[0]))
+    # the perimeter indices have rgb code (0, 255, 0)
+    perimIndices = np.where(np.all(traceImage == [0, 255, 0], axis=-1))
+    coords = np.column_stack((perimIndices[1], perimIndices[0]))
 
-mask = np.zeros((traceImage.shape[0], traceImage.shape[1]))
-cv2.fillConvexPoly(mask, coords, 1)
-#mask = mask.astype(np.bool)
-plt.imshow(mask)
-plt.xticks([]), plt.yticks([])
-plt.show()
+    mask = np.zeros((traceImage.shape[0], traceImage.shape[1]))
+    cv2.fillConvexPoly(mask, coords, 1)
+    mask = 255*(1-mask)
 
-cv2.imwrite('maskmaybe1.png', 255*(1-mask));
+    # cv2.imwrite('mask1.png', mask);
+    perimMask = np.zeros((traceImage.shape[0], traceImage.shape[1]));
+    perimMask[perimIndices] = 255;
+    perimMask = cv2.threshold(perimMask, 1, 255, cv2.THRESH_BINARY_INV)[1]
+
+    ### COLOUR REGION OUTSIDE IMAGES CONTOUR WHITE ###############################
+
+    # load photo image
+    photoImagePath = os.path.join(pathPhotos, photoFiles[jj])
+    photoImage = cv2.imread(photoImagePath)
+
+    # make the outside of the photo be white
+    img = photoImage.copy()
+    photoImageClean = np.ones_like(img)*255 
+    photoImageClean[:,:,0][mask==0]=img[:,:,0][mask==0]
+    photoImageClean[:,:,1][mask==0]=img[:,:,1][mask==0]
+    photoImageClean[:,:,2][mask==0]=img[:,:,2][mask==0]
+
+    # load trace as grayscale
+    traceImagePath = os.path.join(pathTraces, traceFiles[jj])
+    traceBWImage = cv2.imread(traceImagePath, cv2.IMREAD_GRAYSCALE)
+    # obtain the minimum nonzero grayscale value
+    grayscaleValues = np.unique(traceBWImage);
+    minGrayscaleValue = min(grayscaleValues[1:len(grayscaleValues)]);
+    # make trace black and white
+    traceBWImage = cv2.threshold(traceBWImage, minGrayscaleValue-1, 255, cv2.THRESH_BINARY_INV)[1]
+
+    # make the outside of the trace white and make trace black and white
+    traceImageClean = traceBWImage.copy()
+    traceImageClean[perimMask==0]=255;
+
+    ### CROP ###################################################################
+
+    x = min(perimIndices[0]) 
+    y = min(perimIndices[1])
+    w = max(perimIndices[0])-x
+    h = max(perimIndices[1])-y
+
+    # crop
+    photoImageCropped = photoImageClean[x:x+w,y:y+h]
+    traceImageCropped = traceImageClean[x:x+w,y:y+h]
+
+    ### SAVE ###################################################################
+
+    # write to file
+    cv2.imwrite(os.path.join(pathPhotosProcessed, traceFiles[jj]), photoImageCropped);
+    cv2.imwrite(os.path.join(pathTracesProcessed, traceFiles[jj]), traceImageCropped);
