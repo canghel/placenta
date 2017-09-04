04b40dfa6254baa9a9838c7d3969d96fbe14a27b
canghel <cvanghel@gmail.com>
2017-09-04 10:29:56 -0700

    change paths to compute on new images, add np library that was missing, add 0.0 to integer to make it a float to be able to take sqrt

--- scripts/calculate.mcc.py
+++ scripts/calculate.mcc.py
@@ -8,19 +8,24 @@ import cv2
 import os
 from os import listdir
 from os.path import isfile, join
-import glob
 import re
-
+import fnmatch
 from sklearn import preprocessing
 
-# import scipy.misc
+import scipy.misc
+import numpy as np
 
 ### PATHS #####################################################################
 
-pathResults = "../../../pytorch-CycleGAN-and-pix2pix/results/placenta_pix2pix/2017-08-20-test_latest/images/"
-# get list of fake files and of real files
-fakeFiles = [ff for file in sorted(os.listdir(pathResults)) for ff in re.findall("*fake_B.png", file)];
-realFiles = [re.sub("fake_B", "real_B", x) for x in fakeFiles]
+#pathResults = "../../../pytorch-CycleGAN-and-pix2pix/results/placenta_pix2pix/2017-08-20-test_latest/images/"
+## get list of fake files and of real files
+#fakeFiles = [ff for file in sorted(os.listdir(pathResults)) for ff in re.findall("*fake_B.png", file)];
+#realFiles = [re.sub("fake_B", "real_B", x) for x in fakeFiles]
+
+pathReal = "/home/Documents/placenta/data/Reconstructed/CroppedTrace/";
+pathFake = "/home/Documents/placenta/data/Reconstructed/CroppedAverage/"
+fakeFiles = fnmatch.filter(os.listdir(pathFake), '*_recon_avg.png');
+realFiles = [re.sub("_recon_avg.png", "", x) for x in fakeFiles]
 
 numFiles = len(fakeFiles)
 mccResults = []
@@ -29,13 +34,13 @@ for jj in range(0, numFiles):
 	print('--- Working on file '+str(jj+1)+' --------------------------------')
 	
 	# read in the images
-	fakeImagePath = os.path.join(pathResults, fakeFiles[jj])
+	fakeImagePath = os.path.join(pathFake, fakeFiles[jj])
 	fakeImage = scipy.misc.imread(fakeImagePath, mode="L");
-	realImagePath = os.path.join(pathResults, realFiles[jj])
+	realImagePath = os.path.join(pathReal, realFiles[jj])
 	realImage = scipy.misc.imread(realImagePath, mode="L");
-	fakeImagePath = os.path.join(pathResults, fakeFiles[jj]);
+	fakeImagePath = os.path.join(pathFake, fakeFiles[jj]);
 	fakeImage = cv2.imread(fakeImagePath, cv2.IMREAD_GRAYSCALE);
-	realImagePath = os.path.join(pathResults, realFiles[jj]);
+	realImagePath = os.path.join(pathReal, realFiles[jj]);
 	realImage = cv2.imread(realImagePath, cv2.IMREAD_GRAYSCALE);
 	
 	## checked where to have a cutoff for black and white
@@ -63,5 +68,5 @@ for jj in range(0, numFiles):
 	TN = len(np.intersect1d(np.where(realImageBW.ravel()==0), np.where(fakeImageBW.ravel()==0)));
 	FP = len(np.intersect1d(np.where(realImageBW.ravel()==0), np.where(fakeImageBW.ravel()==1)));
 	FN = len(np.intersect1d(np.where(realImageBW.ravel()==1), np.where(fakeImageBW.ravel()==0)));
-	mcc = (TP*TN-FP*FN)/np.sqrt((TP+FP)*(TP+FN)*(TN+FP)*(TN+FN));
+	mcc = (TP*TN-FP*FN)/np.sqrt(((TP+FP)*(TP+FN)*(TN+FP)*(TN+FN))+0.0);
 	mccResultsCheck.append(mcc); 
